ec260f947221264eb04191ee86d7cdec2a0c4bf0
bianca-loera <bianca.loera.062@my.csun.edu>
2025-03-11 11:58:54 -0700

    Add spring boot project files

---
+++ demo/src/main/java/comp440/demo/PublicationProjectApplication.java
@@ -0,0 +1,13 @@
+package comp440.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicationProjectApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/comp440/demo/PublicationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package comp440.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
